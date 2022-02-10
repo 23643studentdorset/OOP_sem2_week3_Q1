70af54ea9638e5e2013a8354fc8e8b4780d6b423
23643studentdorset <23643@student.dorset-college.ie>
2022-02-10 15:07:34 +0000

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace OOP_sem2_week3_Q1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /* Write a program for a package delivery service. 
+             * The program contains an array that holds the ten zip codes to which the company delivers packages. 
+             * Prompt a user to enter a zip code and display a message indicating whether the zip code is one to which the company delivers.
+             */
+
+            string[] zipCodes = { "IRE4525", "UKK8690", "CHL7480", "GER7561", "BRZ948", "ARG295", "USA9563", "MEX5633", "COL5925", "FRA8593" };
+            
+            Console.WriteLine("Enter your zip code");
+            string input = Console.ReadLine();
+
+            foreach (string i in zipCodes)
+            {
+                if (i.Equals(input))
+                {
+                    Console.WriteLine("We deliver to your city");
+                }
+            }
+            Console.WriteLine("Sorry, we dont deliver to your city yet");
+        }
+    }
+}
